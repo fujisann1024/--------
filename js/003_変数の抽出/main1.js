043ea6dc285dbb70f0770f3c667f59bcb8353316
fujisann1024 <tehepero302@gmail.com>
2021-11-22 00:51:05 +0900

    ＃変数の抽出(リファクタリング後)

--- js/003_変数の抽出/main1.js
+++ js/003_変数の抽出/main1.js
@@ -1,5 +1,12 @@
 function price(order){
-    return order.quantity * order.itemPrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(order.quantity * order,itemPrice * 0.1, 100) ;
+    //価格 = 数量 * 単価
+    const basePrice = order.quantity * order.itemPrice; 
+
+    //数量割引　= 数量から500を超えた分の価格を5パーセント引き
+    const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+
+    //送料 =　価格 * 10％ ※価格が1000以上であれば送料100円で固定
+    const shopping =    Math.min(basePrice * 0.1, 100);
+    
+    return basePrice -　quantityDiscount + shopping;
 }
