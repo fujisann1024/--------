d11c33e94672988efb04bced004806cf954c41e8
fujisann1024 <tehepero302@gmail.com>
2021-11-22 01:45:02 +0900

    #パラメータの追加

--- js/005_関数宣言の変更/main1.js
+++ js/005_関数宣言の変更/main1.js
@@ -1,12 +1,12 @@
 ///////////////簡易な手順/////////////////////
-//周囲
-function cricum(radius){
+//円周
+function cricumference(radius){
     return 2 * Math.PI * radius;
 }
 ///////////////簡易な手順/////////////////////
 
 ///////////////移行的手順/////////////////////
 function cricum(radius){
-    return 2 * Math.PI * radius;
+   return cricumference(radius);
 }
 ///////////////移行的手順/////////////////////
