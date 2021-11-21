ad1c979a4717b0d18740450a7997355283b835b4
fujisann1024 <tehepero302@gmail.com>
2021-11-22 02:04:21 +0900

    ＃リファクト2

--- js/005_関数宣言の変更/main2.js
+++ js/005_関数宣言の変更/main2.js
@@ -1,11 +1,7 @@
 class Book{
     //利用者への貸出予約を受け付けるメソッド
     //→パラメータを追加して優先度付き待ち行列をサポートする
-    addReservation(customer){
-        this.zz_addReservation(customer,false);
-    }
-
-    zz_addReservation(customer,isPriority){
+    addReservation(customer,isPriority){
         //アサーションは常に真であることを前提とした条件分
         assert(isPriority === true || isPriority === false);
         this._reservations.push(customer);
