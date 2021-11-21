9bacec1d237c1a371eb56d63f32dce65e351bf6c
fujisann1024 <tehepero302@gmail.com>
2021-11-21 22:24:41 +0900

    #関数の抽出(リファクタリング後)

--- js/001_関数の抽出/main1.js
+++ js/001_関数の抽出/main1.js
@@ -1,22 +1,34 @@
 //スコープ外となる変数が存在しない場合
 function printOwing(invoice){
-    let outstanding = 0;
 
+    printBanner()                         //バナー表示
+    outstanding = recordDueDate(invoice); //未払い金の計算
+    recordDueDate(invoice);               //締め切り日の記録
+    printDetails(invoice, outstanding);   //明細の印字
+    
+}
+
+function printBanner(){
     console.log("******************");
     console.log("***Customer Ower***");
-    console.log("******************");
+    console.log("******************");    
+}
 
-    //未払い料金の計算
+function calculate(invoice){
+    let outstanding = 0;
     for(const o of invoice.orders){
         outstanding += o.amount;
     }
+    return outstanding;
+}
 
-    //締め切り日の記録
+function recordDueDate(invoice){
     const today = Clock.today;//システム時間の呼び出しをラップ
     invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+}
 
-    //明細の印字
+function printDetails(invoice, outstanding){
     console.log(`name: ${invoice.customer}`);
     console.log(`amount: ${outstanding}`);
-    console.log(`due: ${invoice.dueDate.toLocalDateString()}`);   
+    console.log(`due: ${invoice.dueDate.toLocalDateString()}`);     
 }
