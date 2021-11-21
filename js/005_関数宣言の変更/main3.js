9e988e73603d9864668c96642d4aab55409e41cb
fujisann1024 <tehepero302@gmail.com>
2021-11-22 03:54:29 +0900

    関数宣言の変更(リファクタリング後)

--- js/005_関数宣言の変更/main3.js
+++ js/005_関数宣言の変更/main3.js
@@ -1,7 +1,17 @@
 //顧客が関西出身か判定する関数
-function inKansai(aCustomer){
-    return ["大阪","京都","滋賀","兵庫","和歌山","奈良"].includes(aCustomer.address.state)
+// function inKansai(aCustomer){
+//     //①変数の抽出
+//     //const stateCode = aCustomer.address.state;
+//     //③変数のインライン化
+//     return xx_inKansai(aCustomer.address.state);
+// }
+//②関数の抽出
+//⑤関数宣言の変更(xx_inKansai→inKansai)
+function inKansai(stateCode){
+    return ["大阪","京都","滋賀","兵庫","和歌山","奈良"].includes(stateCode)
 }
 
 //呼び出し側
-const newKansais = someCosutomers.filter(c => inKansai(c));
+//④関数のインライン化
+//⑤関数宣言の変更(xx_inKansai→inKansai)
+const newKansais = someCosutomers.filter(c => inKansai(c.address.stateCode));
