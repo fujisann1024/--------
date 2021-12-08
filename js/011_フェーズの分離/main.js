52523ace00434d4e5f3a1df4b1efc81b423ea091
fujisann1024 <tehepero302@gmail.com>
2021-12-09 01:04:01 +0900

    #フェーズの分離1

--- js/011_フェーズの分離/main.js
+++ js/011_フェーズの分離/main.js
@@ -2,9 +2,15 @@ function priceOrder(product, quantity, shippingMethod){
     const basePrice = product.basePrice * quantity; //価格
     const discount = Math.max(quantity * product.discountThreshold, 0)
                     * product.basePrice * product.discountRate//値引き額
-    const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
+    const priceData = {basePrice: basePrice};
+    const price = applyShipping(priceData,basePrice,shippingMethod,quantity,discount);
+    return price;
+}
+
+function applyShipping(priceData,shippingMethod,quantity,discount){
+    const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
                     ? shippingMethod.discountedFee : shippingMethod.feePerCase;
     const shippingCost = quantity * shippingPerCase;
-    const price = basePrice - discount * shippingCost;
+    const price = priceData.basePrice - discount * shippingCost;
     return price;
 }
