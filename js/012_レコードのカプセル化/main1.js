4c4866829fceb5f75a285bb3c0c23eac11b7cdf3
fujisann1024 <tehepero302@gmail.com>
2021-12-09 23:49:24 +0900

    #レコードのカプセル化

---
+++ js/012_レコードのカプセル化/main1.js
@@ -0,0 +1,9 @@
+const orgnaization = {name: "Acme Gooseberries", country: "GB"}
+
+//View
+var result
+result = `<h1>${orgnaization.name}</h1>`;
+
+//Controller
+var newName = "NY";
+orgnaization.name = newName
