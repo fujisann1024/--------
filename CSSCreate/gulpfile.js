3fe591f2a3925658127d1650957f2590ac1945ce
fujisann1024 <tehepero302@gmail.com>
2021-12-21 02:42:37 +0900

    #ヘッダー作成

--- CSSCreate/gulpfile.js
+++ CSSCreate/gulpfile.js
@@ -1,13 +1,8 @@
-var gulp  = require("gulp");
-var sass = require('gulp-sass');
-
-// Sassのコンパイル処理
-gulp.task('sass', function(){
-  console.log("Hello World");
-  //scssディレクトリの指定
-  gulp.src('./css/scss/*.scss')
-  //コンパイル実行
-  .pipe(sass({style : 'expanded'})) //出力形式の種類　#nested, compact, compressed, expanded.
-  //出力先の指定
-  .pipe(gulp.dest('css'));
-});
+const { src, dest } = require( 'gulp' );
+const sass = require( 'gulp-sass' )(require('sass'));
+ 
+exports.default = function() {
+	return src( './css/*.scss' )
+		.pipe( sass() )
+		.pipe( dest( './css/' ) );
+}
